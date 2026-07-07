66c8dccaea8daa4ca8f9e54a0f1f4a76171674a0
Miyata <mtaisei1023@gmail.com>
2026-07-07 11:05:49 +0900

    test

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
     <title>時事ニュースまとめ</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    @vite(['resources/css/app.css', 'resources/js/app.ts'])
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 <body>
     <div class="app-layout">
