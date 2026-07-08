8030450d4ac852827ef1619c3a0e4144ca5979aa
Miyata <mtaisei1023@gmail.com>
2026-07-08 10:34:32 +0900

    レイアウト調整

--- resources/views/forum/forum-top.blade.php
+++ resources/views/forum/forum-top.blade.php
@@ -97,11 +97,11 @@
                 @endforelse
             </section>
 
-            <button class="fab-button">
+            <a href="{{ route('forum.create') }}" class="fab-button" aria-label="投稿を作成する">
                 <svg class="fab-icon" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                     <path stroke-linecap="round" stroke-linejoin="round" stroke-width="1.5" d="M12 4v16m8-8H4" />
                 </svg>
-            </button>
+            </a>
 
         </main>
 
