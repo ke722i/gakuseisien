eb50ff4d1f2266209d8410b288f8c44098b0b87b
Miyata <mtaisei1023@gmail.com>
2026-07-08 09:32:29 +0900

    ルートをコントローラーに接続(TOP,新規投稿)

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ForumController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -10,3 +11,7 @@ Route::get('/', function () {
 Route::get('/gakunai-qna', function () {
     return view('qna'); // qna.blade.php を呼び出す
 })->name('gakunai.qna');
+
+
+// 掲示板画面ルート設定(miyata)
+Route::get('/forum-top', [ForumController::class, 'index'])->name('forum.top');
