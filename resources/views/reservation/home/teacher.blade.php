c70737345557e0646b89d8b411283d6df2b8864e
kimura <s2370196@s.do-johodai.ac.jp>
2026-07-08 09:57:43 +0900

    Completion of the vacant classroom home

--- resources/views/reservation/home/teacher.blade.php
+++ resources/views/reservation/home/teacher.blade.php
@@ -1,12 +1,14 @@
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <title>空き教室予約</title>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
-    @vite(['resources/css/home/teacher.css', 'resources/css/reservation/home/teacher.css',])
+    @vite(['resources/css/app.css','resources/css/reservation/home/teacher.css','resources/js/app.js'])
 </head>
+
 <body>
     <div class="app-layout">
 
